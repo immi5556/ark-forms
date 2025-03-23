59ce14724ca54bd0673702bd8b3bb68f77196922
Immanuel Raj <kimr@solar.dk>
2025-03-23 15:50:06 +0530

    more changes.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ark-forms
  dynamic forms
+
+
+# new lib ark-forms
+- used s a smiple reference to geenrate forms, stores & retries from sqlite
---
+++ ark.forms/ark.forms/ExtnCls.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+
+namespace ark.forms
+{
+    public static class ExtnCls
+    {
+        public static IServiceCollection AddArkForm(this IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                    .AddApplicationPart(typeof(ExtnCls).Assembly);
+            return services;
+        }
+
+        public static IApplicationBuilder UseArkForm(this IApplicationBuilder app)
+        {
+            var assembly = typeof(ExtnCls).Assembly;
+            var wwwrootPath = Path.Combine(Path.GetDirectoryName(assembly.Location), "wwwroot");
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(wwwrootPath),
+                RequestPath = "/ark.forms"
+            });
+
+            return app;
+        }
+    }
+}
